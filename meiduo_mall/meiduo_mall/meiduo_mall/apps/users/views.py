5dd761b8b9253156e72206c494ff3183d1aa64aa
zhangxinyue <starmoon_zhang@163.com>
2020-04-17 01:19:17 +0800

    用户名重复注册

--- meiduo_mall/meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from users.models import User
+from django.http import JsonResponse
 
 # Create your views here.
+class UsernameCountView(View):
+    def get(self, request, username):
+        try:
+            count = User.objects.filter(username=username).count()
+
+        except Exception as e:
+            return JsonResponse({'code':400, 'errmsg':'访问数据库失败'})
+
+        return JsonResponse({'code':0, 'errmsg':'ok', 'count':count})
+
+
+
+
